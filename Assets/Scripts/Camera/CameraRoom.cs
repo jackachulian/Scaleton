9b2f2555b8752e85975d9e666bd33fafa3dec75f
Jackachulian <jackachulian@gmail.com>
2023-11-05 00:39:31 -0500

    Room camera snapping

---
+++ Assets/Scripts/Camera/CameraRoom.cs
@@ -0,0 +1,20 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraRoom : MonoBehaviour {
+    [SerializeField] private CinemachineVirtualCamera virtualCam;
+
+    private void Start() {
+        if (virtualCam.Follow == null) {
+            virtualCam.Follow = GameObject.Find("Player").transform;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        virtualCam.enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        virtualCam.enabled = false;
+    }
+}
