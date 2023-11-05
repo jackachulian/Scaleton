17537f0a78257cf92b7e8d472f20b4ab0a46423c
Justout <jkajustin@gmail.com>
2023-11-04 19:16:02 -0500

    You can grab (and can't let go)

--- Assets/Scripts/GrabBox.cs
+++ Assets/Scripts/GrabBox.cs
@@ -6,10 +6,11 @@ public class GrabBox : MonoBehaviour
 {
     private GameObject toGrab;
     public GameObject grabbedBox;
-    void OnTriggerEnter2D(Collision2D c){
+    public GameObject grabPoint;
+    void OnTriggerEnter2D(Collider2D c){
         Debug.Log("SOMETHING ENTERED");
-        if(toGrab != null && grabbedBox == null){
-            if(c.gameObject.GetComponent("isGrabbable") != null){
+        if(toGrab == null && grabbedBox == null){
+            if(c.gameObject.GetComponent("isGrabable") != null){
                 Debug.Log("Grabbable object entered!");
                 toGrab = c.gameObject;
             }
@@ -18,12 +19,18 @@ public class GrabBox : MonoBehaviour
             }
         }
         else{
-            Debug.Log("An object entered, but you are already grabbing!");
+            if(grabbedBox != null){
+                Debug.Log("An object entered, but you are already grabbing!");
+            }
+            if (toGrab != null){
+                Debug.Log("An object entered, but one is already in range!");
+            }
         }
     }
 
-    private void OnCollisionExit2D(Collision2D c){
+    private void OnTriggerExit2D(Collider2D c){
         if(c.gameObject == toGrab){
+            Debug.Log("Grabbable left range!");
             toGrab = null;
         }
     }
@@ -35,6 +42,7 @@ public class GrabBox : MonoBehaviour
         if(CanGrab()){
             grabbedBox = toGrab;
             toGrab = null;
+            grabbedBox.GetComponent<Rigidbody2D>().isKinematic = true;
             grabbedBox.transform.parent = transform;
             Debug.Log("Grab!");
         }
@@ -42,4 +50,11 @@ public class GrabBox : MonoBehaviour
             Debug.Log("Can't grab!");
         }
     }
+
+    public void Update(){
+        if(grabbedBox != null){
+            grabbedBox.transform.position = Vector2.MoveTowards(grabPoint.transform.position,grabPoint.transform.position,Time.deltaTime*100);
+        }
+    }
+
 }
