3eb1da84faff618b7e76db73adeface70f5bf301
Jackachulian <jackachulian@gmail.com>
2023-11-06 07:12:14 -0600

    Save data fix & physics adjust

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
 
 public class SaveData {
-    public static List<InventoryItem> inventory = new List<InventoryItem>();
+    public static List<InventoryItem> inventory;
+
+    static SaveData() {
+        inventory = new List<InventoryItem>();
+    }
 }
