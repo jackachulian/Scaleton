58d850f3acd326105cba7f61f8714665ed63c8da
Jackachulian <jackachulian@gmail.com>
2023-11-13 06:33:50 -0600

    breakable block respawn fix if no shatter

--- Assets/Scripts/BreakableBlock.cs
+++ Assets/Scripts/BreakableBlock.cs
@@ -24,7 +24,7 @@ public class BreakableBlock : Respawnable
 
     void Break(Vector2 force) {
         if (!breakShatterPrefab) {
-            Destroy(gameObject); 
+            gameObject.SetActive(false);
             return;
         }
         
