e12817fa65862f3b825cc4f3ec0c29ea3656ebd4
Jackachulian <jackachulian@gmail.com>
2023-11-16 23:27:44 -0600

    visual improvements

--- Assets/Scripts/LockBox.cs
+++ Assets/Scripts/LockBox.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class LockBox : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log(other);
         FollowingKey key;
         if (other.TryGetComponent(out key)) {
             key.Unlock(this);
